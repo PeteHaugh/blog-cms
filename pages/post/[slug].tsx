71a8650eb9a450d14601c78f8732d7562f81ce71
Peter Haugh <peter.ahaugh@gmail.com>
2022-10-04 10:52:19 +0100

    Added form for comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,12 +4,26 @@ import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
 import PortableText from "react-portable-text";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
 interface Props {
   post: Post;
 }
 
 function Post({ post }: Props) {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
   return (
     <main>
       <Header />
@@ -44,26 +58,57 @@ function Post({ post }: Props) {
             dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
             projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
             content={post.body}
-            serializers={
-              {
-                h1: (props: any) => {
-                  <h1 className="text-2xl font-bold my-5" {...props} />
-                },
-                h2: (props: any) => {
-                  <h2 className="text-xl font-bold my-5" {...props} />
-                },
-                li: ({ children }: any) => {
-                  <li className="ml-4 list-disc" >{children}</li>
-                },
-                link: ({ href, children }: any) => {
-                  <a href={href} className="text-2xl font-bold my-5" >
-                    {children}
-                  </a>
-                }
-              }
-            }
+            serializers={{
+              h1: (props: any) => {
+                <h1 className="text-2xl font-bold my-5" {...props} />;
+              },
+              h2: (props: any) => {
+                <h2 className="text-xl font-bold my-5" {...props} />;
+              },
+              li: ({ children }: any) => {
+                <li className="ml-4 list-disc">{children}</li>;
+              },
+              link: ({ href, children }: any) => {
+                <a href={href} className="text-2xl font-bold my-5">
+                  {children}
+                </a>;
+              },
+            }}
           />
         </div>
+
+        <hr className="max-w-lg my-5 mx-auto border-yellow-500" />
+
+        <form className="flex flex-col p-10 max-w-2xl mx-auto mb-10">
+          <h3 className="text-sm text-yellow-500">Enjoyed this recipe?</h3>
+          <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+          <hr className="py-3 mt-2" />
+          <label className="block mb-5 ">
+            <span className="text-gray-700 ">Name</span>
+            <input
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              placeholder="Your name"
+              type="text"
+            />
+          </label>
+          <label className="block mb-5 ">
+            <span className="text-gray-700 ">Email</span>
+            <input
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              placeholder="Your name"
+              type="text"
+            />
+          </label>
+          <label className="block mb-5 ">
+            <span className="text-gray-700 ">Comment</span>
+            <textarea
+              className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring
+              "
+              placeholder="Your name"
+              rows={8}
+            />
+          </label>
+        </form>
       </article>
     </main>
   );
