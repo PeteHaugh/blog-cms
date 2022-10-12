f04c30d61fa14131635df962bdd2972c85c3f50a
Peter Haugh <peter.ahaugh@gmail.com>
2022-10-12 22:37:53 +0100

    Updated header

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,14 +24,10 @@ export default function Home({ posts }: Props) {
         <div className="px-10 space-y-5">
           <h1 className="text-6xl max-w-xl font-serif ">
             <span className="underline decoration-black decoration-4">
-              Medium
+              The Greedy Ginger
             </span>{" "}
-            is a place to write, read and connect
           </h1>
-          <h2>
-            It's easy and free to post your thinking on any topic and connect
-            wth millions of readers
-          </h2>
+          <h2>Ideas and recipes from one cook to another</h2>
         </div>
 
         <img
